f92dfe12ac142e09f152e7022a021ed7be7fecf7
yntymak <kubanychevyntymak21@gmail.com>
2024-11-28 23:14:37 +0600

    fixed errors for deploy

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ServeStaticModule } from '@nestjs/serve-static'
-import { join } from 'path'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
-	// Устанавливаем глобальный префикс для API
 	app.setGlobalPrefix('api')
-
-	// Включаем CORS
 	app.enableCors()
-
-	// Обслуживание статических файлов из папки 'public'
-	app.useStaticAssets(join(__dirname, '..', 'public'))
 
 	await app.listen(4200)
 }
-
 bootstrap()
