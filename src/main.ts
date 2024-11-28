5347ab2ea53aaa838f24188993c49e75c9cb4f6c
yntymak <kubanychevyntymak21@gmail.com>
2024-11-28 22:55:52 +0600

    fix public folder

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors();
-  await app.listen(4200);
+	const app = await NestFactory.create(AppModule)
+
+	// Устанавливаем глобальный префикс для API
+	app.setGlobalPrefix('api')
+
+	// Включаем CORS
+	app.enableCors()
+
+	// Обслуживание статических файлов из папки 'public'
+	app.useStaticAssets(join(__dirname, '..', 'public'))
+
+	await app.listen(4200)
 }
-bootstrap();
+
+bootstrap()
