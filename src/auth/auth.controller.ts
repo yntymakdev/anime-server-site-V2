0b69753f5ec4edecc986ce2346097617b16f94c8
yntymak <kubanychevyntymak21@gmail.com>
2024-12-01 20:12:45 +0600

    costumized swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,28 +6,42 @@ import {
 	UsePipes,
 	ValidationPipe
 } from '@nestjs/common'
+import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto/auth.dto'
 import { RefreshTokenDto } from './dto/refresh-token.dto'
 
+@ApiTags('Auth') // Категория в Swagger UI
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
+
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('register')
+	@ApiOperation({ summary: 'Register a new user' }) // Описание метода
+	@ApiBody({ type: AuthDto }) // Указание DTO
 	async register(@Body() dto: AuthDto) {
 		return this.authService.register(dto)
 	}
+
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login')
+	@ApiOperation({ summary: 'Log in a user' }) // Описание метода
+	@ApiBody({ type: AuthDto }) // Указание DTO
 	async login(@Body() dto: AuthDto) {
 		return this.authService.login(dto)
 	}
+
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login/access-token')
+	@ApiOperation({ summary: 'Get new access tokens using refresh token' }) // Описание метода
+	@ApiBody({
+		type: RefreshTokenDto,
+		description: 'Provide a valid refresh token'
+	}) // Указание DTO
 	async getNewTokens(@Body() dto: RefreshTokenDto) {
 		return this.authService.getNewTokens(dto.refreshToken)
 	}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.setGlobalPrefix('api')
+
+	const config = new DocumentBuilder()
+		.setTitle('Movie Website(Anime)')
+		.setDescription('The movie website desription')
+		.setVersion('1.0')
+		.addTag('movie')
+		.build()
+	const documentFactory = () => SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('docs', app, documentFactory)
 	app.enableCors()
 
 	await app.listen(4200)
